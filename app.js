f8aa9346434eb8fff0882d2cc776b5ca6a55de66
mzwae <mrmonjaie@gmail.com>
2018-07-26 16:22:08 +0100

    added events

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Jack',
-    website: 'https://google.com/ncr'
+    website: 'https://google.com/ncr',
+    age: 30
+  },
+  methods:{
+    add: function(inc){
+      this.age += inc;
+    },
+    subtract: function(dec){
+      this.age -= dec;
+    }
   }
 });
