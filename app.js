17ea9c3db84fb70793b843d869dce89012d52a2a
mzwae <mrmonjaie@gmail.com>
2018-07-30 19:21:27 +0100

    added looping through lists

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Jack',
-    website: 'https://google.com/ncr',
-    age: 30
-  },
-  methods:{
-    add: function(inc){
-      this.age += inc;
-    },
-    subtract: function(dec){
-      this.age -= dec;
-    },
-    logName: function(){
-      console.log("You entered Name");
-    },
-    logAge: function(){
-      console.log("You entered Age");
-    }
-  }
+          characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+          ninjas: [
+              { name: 'Ryu', age: 25 },
+              { name: 'Yoshi', age: 35 },
+              { name: 'Ken', age: 55 }
+          ]
+      },
+      methods: {
+
+      },
+      computed: {
+
+      }
 });
